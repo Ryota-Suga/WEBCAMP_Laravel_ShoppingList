1339fb1dfe17bd11cf1e388eaa73ddb6b69a34e7
Ryota-Suga <Ryota-Suga@example.com>
2022-05-26 00:43:12 +0000

    管理画面のユーザ一覧機能を書いた

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -2,11 +2,5 @@
 
 {{-- メインコンテンツ --}}
 @section('contets')
-        <menu label="リンク">
-        <a href>管理画面Top</a><br>
-        <a href>ユーザ一覧</a><br>
-        <a href="/admin/logout">ログアウト</a><br>
-        </menu>
-
         <h1>管理画面</h1>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ShoppingListController;
 
 use App\Http\Controllers\Admin\AuthController as AdminAuthController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -41,6 +42,7 @@ Route::prefix('/admin')->group(function () {
     // 認可処理
     Route::middleware(['auth:admin'])->group(function () {
         Route::get('/top', [AdminHomeController::class, 'top'])->name('admin.top');
+        Route::get('/user/list', [AdminUserController::class, 'list'])->name('admin.user.list');
     });
     // ログアウト
     Route::get('/logout', [AdminAuthController::class, 'logout']);
